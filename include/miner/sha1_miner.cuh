c51bd3bd36e20c8ecc33adeab3d234a731b4bda3
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2025-07-09 21:09:13 -0400

    Update sha1_miner.cuh

--- include/miner/sha1_miner.cuh
+++ include/miner/sha1_miner.cuh
@@ -28,7 +28,7 @@
     // AMD GPUs need different values based on architecture
     // This will be overridden at runtime
     #define NONCES_PER_THREAD 4096
-    #define NONCES_PER_THREAD_RDNA1 4096
+    #define NONCES_PER_THREAD_RDNA1 2048
     #define NONCES_PER_THREAD_RDNA2 4096
     #define NONCES_PER_THREAD_RDNA3 4096
     #define NONCES_PER_THREAD_RDNA4 32768
