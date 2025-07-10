6d4f639d4a39034ea5f9f41072af4faa825185a3
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2025-07-09 20:42:07 -0400

    Update gpu_architecture.hpp

--- src/gpu_architecture.hpp
+++ src/gpu_architecture.hpp
@@ -229,7 +229,7 @@ public:
 
             case AMDArchitecture::RDNA1:
                 blocks_per_cu = 24;
-                config.threads_per_block = 128;  // Reduce from 256 to allow more blocks
+                config.threads_per_block = 64;  // Reduce from 256 to allow more blocks
                 config.num_streams = 12;  // Increase from 8
                 config.result_buffer_size = 2048;  // Increase from 1024
                 config.blocks_per_stream = actual_cus * blocks_per_cu;
