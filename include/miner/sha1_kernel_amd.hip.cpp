81c5244d42fc1662a9aa09e49bda13f2c6963e49
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2025-07-11 03:44:57 -0400

    Update sha1_kernel_amd.hip.cpp

--- include/miner/sha1_kernel_amd.hip.cpp
+++ include/miner/sha1_kernel_amd.hip.cpp
@@ -108,8 +108,6 @@ __global__ void sha1_mining_kernel_amd(
     uint64_t * __restrict__ actual_nonces_processed,
     uint64_t job_version
 ) {
-    printf("run!");
-
     // Thread indices
     const uint32_t tid = blockIdx.x * blockDim.x + threadIdx.x;
     const uint32_t lane_id = threadIdx.x & 63; // 64-thread wavefront
